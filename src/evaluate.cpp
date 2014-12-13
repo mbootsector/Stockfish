06b85b74c3f412432149c53faa384e68abba9afc
mbootsector <mbootsector@gmail.com>
2014-12-13 11:11:02 +0100

    Less king safety.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -91,7 +91,7 @@ namespace {
   // Evaluation weights, indexed by evaluation term
   enum { Mobility, PawnStructure, PassedPawns, Space, KingSafety };
   const struct Weight { int mg, eg; } Weights[] = {
-    {289, 344}, {233, 201}, {221, 273}, {46, 0}, {318, 0}
+    {289, 344}, {233, 201}, {221, 273}, {46, 0}, {286, 0}
   };
 
   typedef Value V;
